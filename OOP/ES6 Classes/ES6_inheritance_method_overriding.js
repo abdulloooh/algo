28b5c95b1a82160b99c15929c8a28dd560cc115b
abdulloooh <abdullahakinwumi@gmail.com>
2020-10-16 16:43:34 +0100

    inheritance and method overiding

---
+++ OOP/ES6 Classes/ES6_inheritance_method_overriding.js
@@ -0,0 +1,34 @@
+class Shape {
+  constructor(thickness) {
+    this.thickness = thickness;
+  }
+  draw() {
+    console.log("drawn");
+  }
+}
+
+class Circle extends Shape {
+  //no need for constructor here if no new value has to be initialized
+  //else, there is need which will then require invoking super()
+
+  constructor(thickness, radius) {
+    super(thickness);
+    this.radius = radius;
+  }
+
+  move() {
+    console.log("moved");
+  }
+
+  //method overriding
+  draw() {
+    //To still use the previous draw method
+    super.draw();
+
+    console.log("draw circle");
+  }
+}
+
+const circle = new Circle(12, 10);
+console.log(circle);
+circle.draw();
