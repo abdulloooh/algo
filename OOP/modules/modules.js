54e63de77bf51417eb5b50a4d45a6613fd0cbff6
abdulloooh <abdullahakinwumi@gmail.com>
2020-10-17 12:27:10 +0100

    babel

--- OOP/modules/modules.js
+++ OOP/modules/modules.js
@@ -4,13 +4,14 @@
  *
  * ES5 Approach
  * = AMD (Asynchronous module definition)   [Browser]
+ * = UMD (Universal Module definition)      [Browser & Node.js]
  * = CommonJs                               [Node.js]
- * = UMD (Universal Module Def)             [Both]
+ *
  * ES6 Approach
  * = ES6 Modules
  *
  * So, focus is now on CommonJs and ES6 modules definition
- * AMD and UMD is now only necessary for devs maintaining LEGACE APPS
+ * AMD and UMD is now only necessary for devs maintaining LEGACY APPS
  *
  * In MODULARITY, COHESION is important which is grouping related things together
  * Content of a module is explicitly private until it's exported
@@ -23,10 +24,10 @@
  * "import" in the main file.
  *
  * In Browser, If Webpack is not being used,it will throw error
- * for destructuring e.g `import {Circle} from ./circle`
+ * for destructuring, not understanding "{" e.g `import {Circle} from ./circle`
  *
  * It has to be speicified in the html file that `type="module"`
- * and relative link yo module has to change to "./circle.js"
+ * & relative link to module has to change to "./circle.js" with the extension ".js"
  *
  * In Node.js, It has some issues too tho 😠
  */
